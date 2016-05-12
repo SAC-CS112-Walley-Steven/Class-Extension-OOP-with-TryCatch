62e43b451ff3a57f288f4195b0f6b065c8f03ef6
Steven Walley <swalley2626@yahoo.com>
2016-05-11 21:06:22 -0700

    Add files via upload

--- TrippleDice.java
+++ TrippleDice.java
@@ -4,8 +4,8 @@ public class TrippleDice extends Dice2 {
 	@Override
 	public int Throw()
 	{
-		int randNum = (super.Throw() + super.Throw() + super.Throw())/3;
-		return randNum;
+		int randNumAverage = (super.Throw() + super.Throw() + super.Throw())/3;
+		return randNumAverage;
 	}
 
 }
