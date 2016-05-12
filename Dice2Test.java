a4123f9de61cc3abf4c9cc3a82bba492b635b076
Steven Walley <swalley2626@yahoo.com>
2016-05-11 21:00:03 -0700

    Add files via upload

--- Dice2Test.java
+++ Dice2Test.java
@@ -7,8 +7,10 @@ public class Dice2Test {
 		Scanner input = new Scanner (System.in);
 		
 		TrippleDice Dice = new TrippleDice();
+		Dice2 dice1 = new Dice2();
 		
-		System.out.print(Dice.Throw());
+		System.out.println(Dice.Throw());
+		System.out.print(dice1.Throw());
 		
 	}
 
