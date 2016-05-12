d6aaf49e37301547aee38ce074ba02e27b1b27a1
Steven Walley <swalley2626@yahoo.com>
2016-05-11 21:08:19 -0700

    Add files via upload

--- ThreeDice.java
+++ ThreeDice.java
@@ -1,15 +1,14 @@
-public class ThreeDice extends Dice2{
+public class ThreeDice {
 	
 	private Dice2 D1, D2, D3;
 	
-	@Override
 	public int Throw()
 	{
 		int average = (D1.Throw(1) + D2.Throw(1) + D3.Throw(1))/3;
 		return average;
 	}
 	
-	@Override
+	
 	public String toString()
 	{
 		return String.format("D1 = %d\nD2 = %d\nD3 = %d", D1, D2, D3);
